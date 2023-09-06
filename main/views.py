2b05434f526d79af68a99da08d10b31007b0caaa
LokSki22 <muhammad.farrel21@ui.ac.id>
2023-09-06 13:37:42 +0700

    Habis Diperbarui

---
+++ main/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+# Create your views here.
+def show_main(request):
+    context = {
+        'name': 'Muhammad Farrel Altaf',
+        'class': 'PBP C'
+    }
+
+    return render(request, "main.html", context)
